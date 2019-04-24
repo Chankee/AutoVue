742fc9841cbb126b6d7bea10294170bd91297a2f
ChanKee <ChanKee@github.com>
2019-04-24 23:29:16 +0800

    register.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/auth/Login'
+import Register from '@/components/auth/Register'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta:{
+          title:"用户登录"
+      }
+    }, {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta:{
+          title:"用户注册"
+      }
     }
   ]
 })
