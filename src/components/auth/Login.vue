f72069d1141312b75f27fd90f07f07a13466f553
kee <18316603168@163.com>
2019-04-25 17:54:30 +0800

    修改

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -2,7 +2,7 @@
     <div class="login">
           <header><h1>TestPlatform</h1></header>
           <div class="submit_form">
-                <form>
+                <form id="login_form">
                       <div class="name">
                           <label><i class="iconfont" style="font-size: 20px">&#xe61c;</i></label>
                           <input type="text" v-model="username" placeholder="请输入账号/邮箱" class="name_inp">
@@ -11,10 +11,10 @@
                           <label><i class="iconfont" style="font-size: 20px">&#xe652;</i></label>
                           <input type="password" v-model="passwd" placeholder="请输入密码" class="name_inp">
                       </div>
-                      <div class="a_link">
-                          <p style="float: left;font-size: 18px"><router-link to="/register" style="font-size:18px;color: #4262aa">注册账号</router-link></p>
-                          <p style="float: right;font-size: 18px"><router-link to="/register" style="font-size:18px;color: #4262aa">忘记密码</router-link></p>
-                      </div>
+
+                      <p style="float: left;font-size: 18px"><router-link to="/register" style="font-size:18px;color: #4262aa">注册账号</router-link></p>
+                      <p style="float: right;font-size: 18px"><router-link to="/register" style="font-size:18px;color: #4262aa">忘记密码</router-link></p>
+
                       <button class="submit" >登录</button>
                 </form>
           </div>
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -5,23 +5,23 @@
             <form id="re">
                   <div class="name">
                        <label><i class="iconfont" style="font-size: 20px">&#xe61c;</i></label>
-                       <input type="text" v-model="username" placeholder="请输入账号" class="name_inp">
+                       <input type="text" v-model="registerForm.username" placeholder="请输入账号" class="name_inp">
                   </div>
                   <div class="name">
                        <label><i class="iconfont" style="font-size: 20px">&#xe668;</i></label>
-                       <input type="text" v-model="mail" placeholder="请输入邮箱" class="name_inp">
+                       <input type="text" v-model="registerForm.mail" placeholder="请输入邮箱" class="name_inp">
                   </div>
                   <div class="name">
                        <label><i class="iconfont" style="font-size: 20px">&#xe652;</i></label>
-                       <input type="password" v-model="passwd" placeholder="请输入密码" class="name_inp">
+                       <input type="password" v-model="registerForm.password" placeholder="请输入密码" class="name_inp">
                   </div>
                   <div class="name">
                        <label><i class="iconfont" style="font-size: 20px">&#xe652;</i></label>
-                       <input type="password" v-model="passwd" placeholder="请确认密码" class="name_inp">
+                       <input type="password" v-model="registerForm.confirmPwd" placeholder="请确认密码" class="name_inp">
                   </div>
 
-                  <button class="submit" >注册</button>
-              <p style="font-size: medium;margin-top: 10px;text-align: center">已有账户，请点击 <router-link to="/login" style="font-size:18px;color: #4262aa">登录</router-link></p>
+                  <button class="submit" @click="submitForm" >注册</button>
+                  <p style="font-size: medium;margin-top: 10px;text-align: center">已有账户，请点击 <router-link to="/login" style="font-size:18px;color: #4262aa">登录</router-link></p>
 
             </form>
         </div>
@@ -32,11 +32,19 @@
     export default {
         name: "Register",
         data () {
-          return {
-            username: '',
-            passwd: '',
-            email:''
-          }
+            return {
+                registerForm:{
+                    username: '',
+                    password: '',
+                    email: '',
+                    confirmPwd:''
+                }
+            }
+        },
+        methods:{
+            submitForm(formName) {
+                alert(123)
+            }
         }
     }
 </script>
